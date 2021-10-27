e6d64f07290d2f0bc05567f234b08c8818257447
Richard Aber <richard.aber@webdevstudios.com>
2021-10-27 15:03:29 -0500

    Add key to list items

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,6 @@ export default function Home() {
 
     const [items, setItems] = useLocalStorageState("items", []);
 
-    function onClick() {
-        setQuery("");
-        setItems([...items, query]);
-    }
-
     function handleSubmit(event) {
         setQuery("");
         setItems([...items, query]);
@@ -20,14 +15,16 @@ export default function Home() {
     }
 
     return (
+        <>
+        <Head>
+            <title>Smoking App</title>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <Head>
-                <title>Smoking App</title>
-                // https://nextjs.org/docs/basic-features/static-file-serving
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
 
             <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+
                 <h1 className="text-3xl font-bold">
                     Welcome to{' '}
                     <a className="text-blue-600" href="https://nextjs.org">
@@ -36,24 +33,28 @@ export default function Home() {
                 </h1>
 
                 <p className="mt-3 text-base">
+                    Currently just a single textarea with a submit button that adds "entries" to local storage.
+                </p>
+
+                <p className="mt-3 text-base">
                     Enter some text below and hit submit.
                 </p>
 
-                <div>
-                    <form onSubmit={handleSubmit}>
-                        <label className="block text-left">
-                            <span className="text-gray-700">This is a textarea</span>
-                            <textarea className="form-textarea mt-1 block w-full border-solid border-4 border-light-blue-500" placeholder="Type something here." value={query} onChange={(e) => setQuery(e.target.value)}/>
-                        </label>
-                        <input type="submit" value="Submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"/>
-                    </form>
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <label className="block text-left">
+                        <span className="text-gray-700">This is a textarea</span>
+                        <textarea className="form-textarea mt-1 block w-full border-solid border-4 border-light-blue-500" placeholder="Type something here." value={query} onChange={(e) => setQuery(e.target.value)}/>
+                    </label>
+                    <input type="submit" value="Submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"/>
+                </form>
 
-                <div>
-                    {items.map((item) => (
-                        <div>{item}</div>//<div dangerouslySetInnerHTML={{__html: item}}></div>
+                <ul>
+                    {items.map((item, index) => (
+                        <li key={`${index}-${item}`}>
+                            {item}
+                        </li>
                     ))}
-                </div>
+                </ul>
 
             </main>
 
@@ -66,9 +67,9 @@ export default function Home() {
                 >
                     Powered by{' '}
                     <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2"/>
-                    // https://nextjs.org/docs/basic-features/static-file-serving
                 </a>
             </footer>
         </div>
+        </>
     )
 }
