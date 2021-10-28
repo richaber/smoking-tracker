735429fbe42c187c0d4b7d52db9fc6876e6b70ef
Richard Aber <richard.aber@webdevstudios.com>
2021-10-27 19:15:50 -0500

    Trying to figure out a little bit about how Tailwind works, or in my case, doesnt

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,101 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import {useState} from "react";
-import useLocalStorageState from 'use-local-storage-state'
+import useLocalStorageState from 'use-local-storage-state';
 
 export default function Home() {
 
+    // useState "Returns a stateful value, and a function to update it."
     const [query, setQuery] = useState("");
 
-    const [items, setItems] = useLocalStorageState("items", []);
+    // useLocalStorageState "Returns [value, setValue, { removeItem, isPersistent }]"
+    const [items, setItems, {removeItem, isPersistent}] = useLocalStorageState("items", []);
 
     function handleSubmit(event) {
         setQuery("");
-        setItems([...items, query]);
+        setItems(
+            [...items, { "ts": Math.round((new Date()).getTime() / 1000), "value": query } ]
+        );
         event.preventDefault();
     }
 
     return (
         <>
-        <Head>
-            <title>Smoking App</title>
-            <link rel="icon" href="/favicon.ico"/>
-        </Head>
+            <Head>
+                <title>Smoking App</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <div>
+                <div class="md:grid md:grid-cols-3 md:gap-6">
 
-            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+                    <h1 className="text-3xl font-bold">
+                        Welcome to Smoking App!
+                    </h1>
 
-                <h1 className="text-3xl font-bold">
-                    Welcome to{' '}
-                    <a className="text-blue-600" href="https://nextjs.org">
-                        Smoking App!
-                    </a>
-                </h1>
+                    <div class="mt-5 md:mt-0 md:col-span-3">
 
-                <p className="mt-3 text-base">
-                    Currently just a single textarea with a submit button that adds "entries" to local storage.
-                </p>
+                        <form onSubmit={handleSubmit}>
 
-                <p className="mt-3 text-base">
-                    Enter some text below and hit submit.
-                </p>
+                            <div class="shadow sm:rounded-md sm:overflow-hidden">
 
-                <form onSubmit={handleSubmit}>
-                    <label className="block text-left">
-                        <span className="text-gray-700">This is a textarea</span>
-                        <textarea className="form-textarea mt-1 block w-full border-solid border-4 border-light-blue-500" placeholder="Type something here." value={query} onChange={(e) => setQuery(e.target.value)}/>
-                    </label>
-                    <input type="submit" value="Submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"/>
-                </form>
+                                <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
 
-                <ul>
-                    {items.map((item, index) => (
-                        <li key={`${index}-${item}`}>
-                            {item}
-                        </li>
-                    ))}
-                </ul>
+                                    <div>
+                                        <label for="about" class="block text-sm font-medium text-gray-700">
+                                            Tell us about your smoking desires
+                                        </label>
+                                        <div class="mt-1">
+                                            <textarea id="smoked" name="smoked" rows="3"
+                                                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                                                      placeholder="What is it you truly desire to smoke?"
+                                                      onChange={(e) => setQuery(e.target.value)}
+                                                      value={query} />
+                                        </div>
+                                        <p class="mt-2 text-sm text-gray-500 text-center">
+                                            Enter a few sentences describing your smoking desires.
+                                        </p>
+                                    </div>
 
-            </main>
+                                </div>
+
+                                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                    <input type="submit" value="Submit"
+                                           className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"/>
+                                </div>
+
+                            </div>
+                        </form>
+
+                    </div>
+
+                    <div className="mt-5 md:mt-0 md:col-span-3">
+                        <div id="cards" className="">
+                            <h2 className="text-center uppercase text-4xl xl:text-5xl">
+                                Entries
+                            </h2>
+                            <div className="container w-100 lg:w-4/5 mx-auto flex flex-col">
+                                {items.map((item, index) => (
+                                    <div key={`${index}-${item.value}`}
+                                        className="flex flex-col md:flex-row overflow-hidden bg-white rounded-lg shadow-xl  mt-4 w-100 mx-2">
+                                        <div className="w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
+                                            <h3 className="font-semibold text-lg leading-tight truncate">TITLE</h3>
+                                            <p className="mt-2">
+                                                {item.value}
+                                            </p>
+                                            <p className="text-sm text-gray-700 uppercase tracking-wide font-semibold mt-2">
+                                                {new Date(item.ts * 1000).toString()}
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))}
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
 
             <footer className="flex items-center justify-center w-full h-24 border-t">
                 <a
@@ -69,7 +108,7 @@ export default function Home() {
                     <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2"/>
                 </a>
             </footer>
-        </div>
+
         </>
     )
 }
